59bc16fbc62e02ee1c50f4098f5b471c87c82063
RARE-1 <rareone2003@gmail.com>
2026-03-20 23:10:38 +0530

    scanning center

--- chatting/lib/features/onboarding/views/onboarding_flow_view.dart
+++ chatting/lib/features/onboarding/views/onboarding_flow_view.dart
@@ -231,6 +231,7 @@ class _OnboardingFlowViewState extends State<OnboardingFlowView> {
     return Padding(
       padding: const EdgeInsets.all(24),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           const SizedBox(height: 24),
           Text(
